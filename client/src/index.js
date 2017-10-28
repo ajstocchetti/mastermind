5aa6ee2ae24e1b9727884575625e41737f285803
tackypinkflamingos <ajstocchetti@gmail.com>
2017-10-28 09:43:03 -0500

    redux logger

--- client/src/index.js
+++ client/src/index.js
@@ -1,3 +1,4 @@
+const logger = require('redux-logger').logger;
 const React = require('react');
 const ReactDOM = require('react-dom');
 const Provider = require('react-redux').Provider;
@@ -5,7 +6,7 @@ const redux = require('redux');
 const App = require('./components/App');
 const reducer = require('./redux/reducer');
 
-let store = redux.createStore(reducer);
+let store = redux.createStore(reducer, redux.applyMiddleware(logger));
 
 window.onload = function() {
     ReactDOM.render(
