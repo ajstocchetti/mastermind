137765a073260644c549a83e95e33369bc585858
tackypinkflamingos <ajstocchetti@gmail.com>
2017-10-30 22:49:36 -0500

    remove helper

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -1,21 +1,15 @@
 const React = require('react');
 const Newgame = require('./newgame');
-const Helper = require('./helper.js');
+// const Helper = require('./helper.js').default;
 const Row = require('./guessrow/guessrow');
+
 class App extends React.Component {
-
-    // constructor(props) {
-    //     super(props);
-    //     this.state = {
-    //     };
-    // }
-
     render() {
         return (
             <div style={{ marginTop: '25px' }}>
                 <h2>MasterMind!</h2>
                 <Newgame />
-                <Row colors={6} spaces={4} />
+                <Row />
             </div>
         );
     };
